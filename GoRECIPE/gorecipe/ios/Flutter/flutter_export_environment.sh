22ae2dcdc75336325325afeaa430db64c112cf2a
Casey Maloney <tuj57107@temple.edu>
2022-02-25 11:36:10 -0500

    adding main home page to the github, only has dashboard trying to fix something

--- GoRECIPE/gorecipe/ios/Flutter/flutter_export_environment.sh
+++ GoRECIPE/gorecipe/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/caseymaloney/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/caseymaloney/Desktop/GoRECIPE/gorecipe"
+export "FLUTTER_APPLICATION_PATH=/Users/caseymaloney/Desktop/Cap_GoRecipe/project-gorecipe/GoRECIPE/gorecipe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/caseymaloney/Desktop/Cap_GoRecipe/project-gorecipe/GoRECIPE/gorecipe/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/caseymaloney/Desktop/Cap_GoRecipe/project-gorecipe/GoRECIPE/gorecipe/.dart_tool/package_config.json"
