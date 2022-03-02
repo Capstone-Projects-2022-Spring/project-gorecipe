633e53003b52fef7fd7f5929f17c2f418da87827
Anna Gillen <tuj50379@temple.edu>
2022-03-01 19:04:19 -0500

    fix buttons and alignment

--- GoRECIPE/gorecipe/lib/Screens/profile.dart
+++ GoRECIPE/gorecipe/lib/Screens/profile.dart
@@ -18,10 +18,18 @@ class _Profile extends State<Profile> {
   @override
   Widget build(BuildContext context){
 
-    const userName = Text(
-      "User Name",
+    const firstName = Text(
+      "First",
       textAlign: TextAlign.center,
-      textScaleFactor: 2.0,
+      textScaleFactor: 2.25,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+    );
+    const lastName = Text(
+      "Last",
+      textAlign: TextAlign.center,
+      textScaleFactor: 2.25,
       style: TextStyle(
         fontWeight: FontWeight.bold,
       ),
@@ -70,103 +78,190 @@ class _Profile extends State<Profile> {
         ),
 
       body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: const AssetImage("assets/images/ingredients.png"),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(.2), BlendMode.dstATop),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage("assets/images/ingredients.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(.2), BlendMode.dstATop),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        height: 200,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage("assets/images/default_pfp.png"),
+                              fit: BoxFit.fill),
+                        ),
+                      ),
+                      const SizedBox(height: 15.0),
+                      firstName,
+                      lastName,
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.black,
+                            size: 24.0,
+                            semanticLabel: 'location symbol',
+                          ),
+                          location,
+                        ],
+                      ),
+
+
+                    ],
+                ),
+
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage("assets/images/default_pfp.png"),
-                            fit: BoxFit.fill),
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  minWidth: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.book,
+                        color: Colors.green,
                       ),
-                    ),
-                    const SizedBox(height: 15.0),
-                    userName,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.black,
-                          size: 24.0,
-                          semanticLabel: 'location symbol',
-                        ),
-                        location,
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 80.0,
-                    ),
-                    SizedBox(
-                      child: FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(
-                            Icons.book
-                        ),
-                        label: Text('MyCookbook', textAlign: TextAlign.left,),
-                        color: Colors.white,
-                        minWidth: double.infinity,
-                      ),
-                    ),
-                    SizedBox(
-                      child: FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(
-                            Icons.wysiwyg_rounded
-                        ),
-                        label: Text('Calendar'),
-                        color: Colors.white,
-                        minWidth: double.infinity,
-                      ),
-                    ),
-                    SizedBox(
-                      child: FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(
-                            Icons.wifi_protected_setup
-                        ),
-                        label: Text('Update Food Preferences'),
-                        color: Colors.white,
-                        minWidth: double.infinity,
-                      ),
-                    ),
-                    SizedBox(
-                      child: FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(
-                            Icons.app_settings_alt
-                        ),
-                        label: Text('Edit Profile'),
-                        color: Colors.white,
-                        minWidth: double.infinity,
-                      ),
-                    )
-                  ],
+                      Text('    MyCookbook', textDirection: TextDirection.ltr, textAlign: TextAlign.justify, textScaleFactor: 1.5,),
+                    ],
+                  ),
+                ),
               ),
-
+              /*child: FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                    Icons.book,
+                    color: Colors.green
+                ),
+                label: Text('MyCookbook', textAlign: TextAlign.left, textScaleFactor: 1.5,),
+                color: Colors.white,
+                minWidth: double.infinity,
+              ),*/
             ),
-
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  minWidth: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.wysiwyg_rounded,
+                        color: Colors.green,
+                      ),
+                      Text('    Calendar', textDirection: TextDirection.ltr, textAlign: TextAlign.justify, textScaleFactor: 1.5,),
+                    ],
+                  ),
+                ),
+              ),
+              /*child: FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                    Icons.wysiwyg_rounded,
+                    color: Colors.green
+                ),
+                label: Text('Calendar', textScaleFactor: 1.5,),
+                color: Colors.white,
+                minWidth: double.infinity,
+              ),*/
+            ),
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  minWidth: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.wifi_protected_setup,
+                        color: Colors.green,
+                      ),
+                      Text('    Update Food Preferences', textDirection: TextDirection.ltr, textAlign: TextAlign.justify, textScaleFactor: 1.5,),
+                    ],
+                  ),
+                ),
+              ),
+              /*child: FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                    Icons.wifi_protected_setup,
+                    color: Colors.green
+                ),
+                label: Text('Update Food Preferences', textScaleFactor: 1.5,),
+                color: Colors.white,
+                minWidth: double.infinity,
+              ),*/
+            ),
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  minWidth: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.app_settings_alt,
+                        color: Colors.green,
+                      ),
+                      Text('    Edit Profile', textDirection: TextDirection.ltr, textAlign: TextAlign.justify, textScaleFactor: 1.5,),
+                    ],
+                  ),
+                ),
+              ),
+              /*child: FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                    Icons.app_settings_alt,
+                    color: Colors.green
+                ),
+                label: Text('Edit Profile', textScaleFactor: 1.5,),
+                color: Colors.white,
+                minWidth: double.infinity,
+              ),*/
+            ),
+            ],
           ),
-        )
-      )
-
-    );
+        ),
+      );
   }
 
 }
