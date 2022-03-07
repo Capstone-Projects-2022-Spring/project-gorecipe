d5f21963559e5afa13a9e5fcf9f6f488f8df10df
Seanwilliams2 <tui27140@temple.edu>
2022-03-07 11:29:52 -0500

    Taking picture works

--- GoRECIPE/gorecipe/lib/main.dart
+++ GoRECIPE/gorecipe/lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:gorecipe/Screens/welcome_screen.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() {
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:gorecipe/Screens/scan_screen.dart';
+
+/*void main() {
   runApp(const MyApp());
+}
+*/
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
