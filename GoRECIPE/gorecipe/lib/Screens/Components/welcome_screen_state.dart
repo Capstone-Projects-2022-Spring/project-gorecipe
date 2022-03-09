f4dd5c7190ea2f60c6d1f351aa0cdf2f3cc28a77
Casey Maloney <77736692+caseyrmaloney@users.noreply.github.com>
2022-03-09 10:16:19 -0500

    Revert "Welcome page"

--- GoRECIPE/gorecipe/lib/Screens/Components/welcome_screen_state.dart
+++ GoRECIPE/gorecipe/lib/Screens/Components/welcome_screen_state.dart
@@ -7,14 +7,7 @@ import 'package:gorecipe/Screens/home_screen.dart';
 import 'package:gorecipe/Screens/welcome_screen.dart';
 
 // ignore: camel_case_types
-class Welcome_Screen_State extends StatefulWidget {
-  const Welcome_Screen_State({Key? key}) : super(key: key);
-
-  @override
-  State<Welcome_Screen_State> createState() => _Welcome_Screen_State();
-}
-
-class _Welcome_Screen_State extends State<Welcome_Screen_State> {
+class Welcome_Screen_State extends State<WelcomeScreen> {
   TextStyle style = const TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
@@ -60,6 +53,19 @@ class _Welcome_Screen_State extends State<Welcome_Screen_State> {
       ),
     );
 
+    final createAccountButton = MaterialButton(
+      child: Text("Create an account",
+          textAlign: TextAlign.center,
+          style:
+              style.copyWith(color: Colors.black, fontWeight: FontWeight.bold)),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CreateAccount()),
+        );
+      },
+    );
+
     return Scaffold(
         body: Center(
       child: SingleChildScrollView(
@@ -100,6 +106,10 @@ class _Welcome_Screen_State extends State<Welcome_Screen_State> {
                 loginButton,
                 const SizedBox(
                   height: 15.0,
+                ),
+                createAccountButton,
+                const SizedBox(
+                  height: 10.0,
                 ),
               ],
             ),
