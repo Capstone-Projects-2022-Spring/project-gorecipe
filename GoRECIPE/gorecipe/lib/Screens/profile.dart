ba9db8385aa4ebe5d4c8393ccb5b6d51bed4286f
Anna Gillen <tuj50379@temple.edu>
2022-03-13 19:12:00 -0400

    add drop down menu

--- GoRECIPE/gorecipe/lib/Screens/profile.dart
+++ GoRECIPE/gorecipe/lib/Screens/profile.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gorecipe/Screens/edit_account.dart';
 import '../Models/User.dart';
-
 import 'package:http/http.dart' as http;
+import 'package:gorecipe/Screens/scan_home_page.dart';
+import 'package:gorecipe/Screens/want_to_add_ingredient.dart';
+import 'package:gorecipe/Screens/welcome_screen.dart';
+import 'package:gorecipe/Screens/home_screen.dart';
 
 // View profile page
 
@@ -98,7 +101,8 @@ class _Profile extends State<Profile> {
               color: Colors.green,
             ),),
           backgroundColor: Colors.white,
-          actions: <Widget>[
+
+          /*actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.menu),
               iconSize: 50,
@@ -122,8 +126,101 @@ class _Profile extends State<Profile> {
                 ));
               },
             ),
+          ],*/
+        ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text(
+                'GoRecipe',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen(
+                            key: ObjectKey('welcome page'),
+                          )));
+
+                  //idk why this isnt working
+                  //   navigation im confused everything is giving me an error
+                  // onTap: () => HomeScreen(),
+
+                  //   onTap: () {
+                  //   Navigator.pop(context);
+
+                  //},
+                }),
+            const ListTile(
+              leading: Icon(Icons.set_meal),
+              title: Text('Set Food Preference'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.book),
+              title: Text('MyCookBook'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.calendar_today_outlined),
+              title: Text('Calendar'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Profile(
+                            key: ObjectKey('profile page'),
+                            title: 'profile page')));
+              },
+            ),
+            const ListTile(
+              leading: Icon(Icons.help_center),
+              title: Text('Help'),
+            ),
+            // putting the scan oon the dropdown menu for now
+
+            ListTile(
+                leading: const Icon(Icons.camera),
+                title: const Text('Scan'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ScanHomeScreen(
+                            key: ObjectKey(
+                                'want to add this an ingredient?'),
+                          )));
+                }),
+
+            ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Log Out'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const WelcomeScreen(
+                              key: ObjectKey('welcome page'),
+                              title: 'welcome page')));
+                }),
           ],
         ),
+      ),
 
       body: Center(
         child: Column(
