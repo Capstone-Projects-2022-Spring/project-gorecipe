01443714be25b64d0558ef8e31a0ac2cd6130349
Phi Truong <phit8645@gmail.com>
2022-03-14 10:28:05 -0400

    put the actual proper page link

--- GoRECIPE/gorecipe/lib/Screens/scanlookup_ingredient.dart
+++ GoRECIPE/gorecipe/lib/Screens/scanlookup_ingredient.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gorecipe/Screens/scan_screen.dart';
-import 'package:gorecipe/Screens/want_to_add_ingredient.dart';
+import 'package:gorecipe/Screens/search_by_ingredient.dart';
 
 class ScanLookup extends StatefulWidget {
   const ScanLookup({Key? key}) : super(key: key);
@@ -53,8 +53,8 @@ class _ScanLookup extends State<ScanLookup> {
         Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (context) => const WantToAdd(
-                  key: ObjectKey(ScanLookup()), title: "Add Ingredient")),
+              builder: (context) => const SearchByIngredient(
+                  key: ObjectKey(ScanLookup()), title: "Search Ingredient")),
         );
       },
     );
