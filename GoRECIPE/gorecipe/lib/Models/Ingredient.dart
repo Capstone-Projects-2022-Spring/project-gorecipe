c684ecdc6d48cd93655ea10fe4efb9c6a913a20e
Seanwilliams2 <tui27140@temple.edu>
2022-03-27 14:26:38 -0400

    Recipe to list created

---
+++ GoRECIPE/gorecipe/lib/Models/Ingredient.dart
@@ -0,0 +1,29 @@
+import 'dart:core';
+import 'dart:convert';
+
+class Ingredient {
+  final String name;
+
+  const Ingredient({
+    required this.name,
+  });
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+      name: json['name'],
+    );
+  }
+
+  static List ingToList(String jsonData) {
+    List ingredients = (jsonDecode(jsonData) as List)
+        .map((data) => Ingredient.fromJson(data).name)
+        .toList();
+
+    return ingredients;
+  }
+
+  @override
+  String toString() {
+    return 'Ingredient{name: $name}';
+  }
+}
--- GoRECIPE/gorecipe/lib/Models/Recipe.dart
+++ GoRECIPE/gorecipe/lib/Models/Recipe.dart
@@ -1,32 +1,49 @@
 import 'dart:core';
+import 'dart:convert';
+import 'package:gorecipe/Models/Ingredient.dart';
 
 class Recipe {
-  final int id;
   final String content;
-  final String ingredients;
+  final List<dynamic> ingredients;
   final String name;
+  final int prepTime;
+  final String sourceURL;
+  final int spoonacularId;
   final String videoURL;
 
   const Recipe({
-    required this.id,
     required this.content,
     required this.ingredients,
     required this.name,
+    required this.prepTime,
+    required this.sourceURL,
+    required this.spoonacularId,
     required this.videoURL,
   });
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
+    List ingList = Ingredient.ingToList(jsonEncode(json['ingredients']));
     return Recipe(
-      id: json['id'],
       content: json['content'],
-      ingredients: json['ingredients'],
+      ingredients: ingList,
       name: json['name'],
+      prepTime: json['prepTime'],
+      sourceURL: json['sourceURL'],
+      spoonacularId: json['spoonacularId'],
       videoURL: json['videoURL'],
     );
   }
 
+  static List<Recipe> ingToList(String jsonData) {
+    List<Recipe> recipes = (jsonDecode(jsonData) as List)
+        .map((data) => Recipe.fromJson(data))
+        .toList();
+
+    return recipes;
+  }
+
   @override
   String toString() {
-    return 'Recipe{id: $id, content: $content, ingredients: $ingredients, name: $name, videoURL: $videoURL }';
+    return 'Recipe{name: $name, content: $content, ingredients: $ingredients, videoURL: $videoURL }';
   }
 }
